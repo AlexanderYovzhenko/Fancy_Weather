8ed4ee28c30d5939d2493bb22c03c59fff99feb2
Alexander <renyreny125tt@gmail.com>
2023-06-14 11:39:59 +0300

    refactor: update node modules

--- src/js/weather-information.js
+++ src/js/weather-information.js
@@ -36,7 +36,7 @@ const weatherInformationLanguages = {
     apparentTemperature: ['АДЧУВАЕЦЦА ЯК', 'ОЩУЩАЕТСЯ КАК', 'FEELS LIKE'],
     windSpeed: ['ВЕЦЯР', 'ВЕТЕР', 'WIND'],
     humidity: ['ВІЛЬГОТНАСЦЬ', 'ВЛАЖНОСТЬ', 'HUMIDITY'],
-    warningWindowText: ['Калі ласка, увядзіце правільны запыт.', 'Пожалуйста, введите правильный запрос.', 'Please enter a valid request.'],
+    warningWindowText: ['Калі ласка, увядзіце правільны запыт', 'Пожалуйста, введите правильный запрос', 'Please enter a valid request'],
     nameDays: [daysBeFull, daysRuFull, daysEnFull],
     nameDaysAbbreviated: [daysBe, daysRu, daysEn],
     months: [monthsBe, monthsRu, monthsEn]
@@ -76,7 +76,7 @@ async function serverRequest(url, message) {
 function updateWeatherInformation() {
     temperatureNow.innerText = `${changeDegrees(Math.round(data.data[0].temp))}`;
 
-    iconWeather.setAttribute('src', `src/img/icons/animated icon/${data.data[0].weather.icon}.svg`);
+    iconWeather.setAttribute('src', `../src/img/icons/animatedIcons/${data.data[0].weather.icon}.svg`);
 
     weatherDescription.innerText = `${data.data[0].weather.description.toUpperCase()}`;
 
@@ -97,9 +97,9 @@ function updateWeatherInformationThreeDay() {
     changeLanguageDayName(nameTomorrow, 1);
     changeLanguageDayName(nameDayAfterTomorrow, 2);
 
-    iconToday.setAttribute('src', `src/img/icons/animated icon/${data.data[0].weather.icon}.svg`);
-    iconTomorrow.setAttribute('src', `src/img/icons/animated icon/${data.data[1].weather.icon}.svg`);
-    iconDayAfterTomorrow.setAttribute('src', `src/img/icons/animated icon/${data.data[2].weather.icon}.svg`);
+    iconToday.setAttribute('src', `../src/img/icons/animatedIcons/${data.data[0].weather.icon}.svg`);
+    iconTomorrow.setAttribute('src', `../src/img/icons/animatedIcons/${data.data[1].weather.icon}.svg`);
+    iconDayAfterTomorrow.setAttribute('src', `../src/img/icons/animatedIcons/${data.data[2].weather.icon}.svg`);
 }
 
 //change the language of the day name
