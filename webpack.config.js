40b1c5d16a7a5de95070dde1294a838dbac18419
Alexander <renyreny125tt@gmail.com>
2023-06-14 12:01:39 +0300

    feat: add gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   resolve: {
     extensions: ['.js', '.json', '.png', 'ico'],
